a0a0fcbb1a9abf74bff98f13de8a7428c6dbefe8
Gilnei Gonzo <45856866+gcgonzo@users.noreply.github.com>
2023-08-18 16:37:30 -0300

    Header scroll fixed

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,18 @@ toggle.onclick = () => {
   body.classList.remove("body")
   body.classList.toggle("themes-light")
 }
+const sun = document.querySelector(".sun").getAttributeNames("src")[0]
+
+console.log(sun)
+
+// Header Scroll fixed
+
+document.addEventListener("scroll", () => {
+  const header = document.querySelector("header")
+  console.log(header)
+  if (window.scrollY > 500) {
+    header.classList.add("scrolled")
+  } else {
+    header.classList.remove("scrolled")
+  }
+})
